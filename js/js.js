918c522b56b5480e01a907c79dbd97d0532e6566
The13thSpirit <13-spirit@ukr.net>
2016-10-08 01:23:39 +0300

    done project
    done

---
+++ js/js.js
@@ -0,0 +1,53 @@
+(function() {
+  window.addEventListener('scroll', function(event) {
+    var depth, i, layer, layers, len, movement, topDistance, translate3d;
+    topDistance = this.pageYOffset;
+    layers = document.querySelectorAll("[data-type='parallax']");
+    for (i = 0, len = layers.length; i < len; i++) {
+      layer = layers[i];
+      depth = layer.getAttribute('data-depth');
+      movement = -(topDistance * depth);
+      translate3d = 'translate3d(0, ' + movement + 'px, 0)';
+      layer.style['-webkit-transform'] = translate3d;
+      layer.style['-moz-transform'] = translate3d;
+      layer.style['-ms-transform'] = translate3d;
+      layer.style['-o-transform'] = translate3d;
+      layer.style.transform = translate3d;
+    }
+  });
+
+}).call(this);
+
+function renderSection(sectionId) {
+  var allSections = document.getElementsByTagName("section"),
+    bgColor, mobile, classArr, layerArr;
+  window.scrollTo(0,0);
+  for (var i = 0; i < allSections.length; i++) {
+    allSections[i].style.display = "none";
+  }
+
+  if (sectionId == "contacts") {
+    bgColor = "#8B3C06";
+    mobile = "mobileContacts";
+    document.getElementById("contacts").style.display = "block";
+
+  } 
+  else if (sectionId == "about") {
+    bgColor = "#6f7c2c";
+    mobile = "mobileAbout";
+    document.getElementById("about").style.display = "block";
+
+  }
+  document.getElementById("content").style.backgroundColor = bgColor;
+  document.body.style.backgroundColor = bgColor;
+
+  document.getElementById("hero-mobile").className = mobile;
+
+  layerArr = document.getElementById("hero").children;
+  for (var j = 0; j < layerArr.length; j++) {
+    classArr =  document.getElementById(layerArr[j].id).className.split(" ");
+    classArr[0] = sectionId;
+    console.log(classArr, layerArr[j]);
+    document.getElementById(layerArr[j].id).className = classArr.join(" ");
+  }
+}
